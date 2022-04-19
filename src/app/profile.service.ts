e554358e37f110d56b8b1d7f33eb08d6477da19b
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-04-19 06:30:54 +0300

    Add initializer to defined properties

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -8,8 +8,8 @@ import {HttpClient , HttpHeaders} from '@angular/common/http';
 export class ProfileService {
 
   private username: string;
-  private clientid: '8c97c13b750e1df8a66a'; 
-  private clientsecret:'7902c3f63256af7ecfa876e3f1b9336ad97ee2de';
+  private clientid!: '8c97c13b750e1df8a66a'; 
+  private clientsecret!:'7902c3f63256af7ecfa876e3f1b9336ad97ee2de';
 
   constructor(private http:HttpClient) { 
     console.log("service is ready!");
