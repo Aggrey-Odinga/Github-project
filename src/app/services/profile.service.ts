af494f19029dd7846c6f905faa5a4adbb7288f63
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-04-19 21:46:17 +0300

    Add new key

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -9,8 +9,8 @@ export class ProfileService {
 
 
   private username: string;
-  clientid!: '8c97c13b750e1df8a66a';
-  clientsecret!: 'f5701c7c70207ae67d1b9db25e3315d5d53f74a0';
+  clientid!: 'f54c99c3ca413f82c2b7';
+  clientsecret!: 'c05c3f32fa243ad8a59d56d0dac0a0207b4b9653';
 
   constructor(private http: HttpClient) {
     this.username = "Aggrey-Odinga";
